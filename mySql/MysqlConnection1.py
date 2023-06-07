69465ee08c629a0aa6dea6513a9d0e6140ac599d
Mehrdad <rashidi.mehrdad@gmail.com>
2023-06-07 11:49:51 +0330

    eighth Commit use getter and setter instead of private variable in method

--- mySql/MysqlConnection1.py
+++ mySql/MysqlConnection1.py
@@ -7,21 +7,21 @@ class MysqlConnection1:
 
     def __init__(self, host, user, password, database):
         try:
-            self.__connection = mysql.connector.connect(host=host, database=database, user=user, password=password)
-            if self.__connection.is_connected():
+            self.connection = mysql.connector.connect(host=host, database=database, user=user, password=password)
+            if self.connection.is_connected():
                 print('Connected to MySQL database')
-                self.__cursor = self.__connection.cursor()
+                self.cursor = self.connection.cursor()
         except mysql.connector.Error as e:
             print('Reason Error Connect to Database is : ', e)
             exit()
 
     def closeConnection(self, commit=True):
         if commit:
-            self.__connection.commit()
-        self.__connection.close()
+            self.connection.commit()
+        self.connection.close()
 
     def execute(self, sql, params=None):
-        self.__cursor.execute(sql, params=params or ())
+        self.cursor.execute(sql, params=params or ())
 
     @property
     def connection(self):
